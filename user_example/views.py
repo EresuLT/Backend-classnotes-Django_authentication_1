a1bd7396efc8a667586fac023c290e0a7e251446
eResult <eresultolu@gmail.com>
2022-10-30 00:41:08 +0300

    first commit

---
+++ user_example/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, 'user_example/index.html')
+
+
+@login_required
+def special(request):
+    return render(request, "user_example/special.html")
+
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    # alttaki üçü üstteki işlemle aynı
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+# buraya kadar register, burdan sonra login
+        # username = form.cleaned_data.get('username')
+        # password = form.cleaned_data.get('password1')
+
+        # user = authenticate(username='username', password='password')
+        # login(request, user)
+
+        return redirect('home')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)
